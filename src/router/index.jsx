5b56cb5a439f7674141d66cc245795b6c224cebd
dglshenriq <dglsramone@gmail.com>
2026-09-16 17:11:23 -0300

    Cria a página de erro 404

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,24 +1,24 @@
-import { BrowserRouter, Route, Routes } from "react-router"
-import { Register } from "../pages/Register"
-import { ProtectedRoute } from "../components/ProtectedRoute"
-import { BlogPost } from "../pages/BlogPost"
-import { Login } from "../pages/Login"
-import { Logout } from "../pages/Logout"
-import { Feed } from "../pages/Feed"
-import { AuthLayout } from "../layouts/Auth"
-import { AppLayout } from "../layouts/App"
+import { BrowserRouter, Route, Routes } from "react-router";
+import { Register } from "../pages/Register";
+import { ProtectedRoute } from "../components/ProtectedRoute";
+import { BlogPost } from "../pages/BlogPost";
+import { Login } from "../pages/Login";
+import { Logout } from "../pages/Logout";
+import { Feed } from "../pages/Feed";
+import { AuthLayout } from "../layouts/Auth";
+import { AppLayout } from "../layouts/App";
+import { NotFound } from "../pages/NotFound";
 
 export const AppRouter = () => {
-
-    return (
-        <BrowserRouter>
+  return (
+    <BrowserRouter>
       <Routes>
-        <Route path="/auth" element={<AuthLayout />}>
+        <Route path='/auth' element={<AuthLayout />}>
           <Route path='register' element={<Register />} />
           <Route path='login' element={<Login />} />
           <Route path='logout' element={<Logout />} />
         </Route>
-        <Route path="/" element={<AppLayout />}>
+        <Route path='/' element={<AppLayout />}>
           <Route
             path=''
             element={
@@ -35,8 +35,9 @@ export const AppRouter = () => {
               </ProtectedRoute>
             }
           />
+          <Route path="*" element={<NotFound />} />
         </Route>
       </Routes>
     </BrowserRouter>
-    )
-}
+  );
+};
