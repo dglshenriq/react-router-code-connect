5e14a612b1386a1a8f3f23f7a3295b5edb42be57
dglshenriq <dglsramone@gmail.com>
2026-09-17 16:21:05 -0300

    Adiciona pequenas atualizacoes

--- src/pages/Feed/index.jsx
+++ src/pages/Feed/index.jsx
@@ -1,7 +1,18 @@
 import { CardPost } from "../../components/CardPost"
 import styles from './feed.module.css'
+import { useEffect, useState } from "react"
+
+
 
 export const Feed = () => {
+    const [posts, setPosts] = useState([]);
+    
+    useEffect(() => {
+  fetch('http://localhost:3000/blog-posts')
+    .then(response => response.json())
+    .then(data => setPosts(data))
+}, [])
+
     return (
             <main className={styles.grid}>
                 {posts.map(post => <CardPost key={post.slug} post={post} />)}
