1416d46772bd8455bbec3e31cda1c0909e2c0da9
dglshenriq <dglsramone@gmail.com>
2026-09-18 11:07:11 -0300

    Adiciona a funcionalidade de feeed

--- src/pages/BlogPost/index.jsx
+++ src/pages/BlogPost/index.jsx
@@ -1,4 +1,3 @@
-import { posts } from "../Feed/data";
 import styles from "./blogpost.module.css";
 import { ThumbsUpButton } from "../../components/CardPost/ThumbsUpButton";
 import { Author } from "../../components/Author";
@@ -7,19 +6,28 @@ import { CommentList } from "../../components/CommentList";
 import ReactMarkdown from "react-markdown";
 import { useNavigate, useParams } from "react-router";
 import { NotFound } from "../NotFound";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { ModalComment } from "../../components/ModalComment";
 
 export const BlogPost = () => {
   const { slug } = useParams();
+  const [post, setPost] = useState(null);
   const navigate = useNavigate();
-  const post = posts.find((p) => p.slug === slug);
 
   useEffect(() => {
-    if (!post) {
-        navigate('/not-found');
-    }
-}, [navigate, post]);
+    fetch(`http://localhost:3000/blog-posts/slug/${slug}`)
+      .then((response) => {
+        if (response.status === 404) {
+          navigate("/not-found");
+        }
+        return response.json();
+      })
+      .then((data) => setPost(data));
+  }, [slug, navigate]);
+
+  if (!post) {
+    return null;
+  }
 
   return (
     <main className={styles.main}>
