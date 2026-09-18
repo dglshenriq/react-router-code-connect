3642d7dfbc5a8bed8c526ac30b8b91f6227c00af
dglshenriq <dglsramone@gmail.com>
2026-09-18 11:40:07 -0300

    Cria função de curtir posts

--- src/components/CardPost/index.jsx
+++ src/components/CardPost/index.jsx
@@ -1,43 +1,48 @@
-import styles from './cardpost.module.css'
+import styles from "./cardpost.module.css";
 
-import { Author } from "../Author"
-import { ModalComment } from "../ModalComment"
-import { ThumbsUpButton } from "./ThumbsUpButton"
-import { Link } from "react-router"
+import { Author } from "../Author";
+import { ModalComment } from "../ModalComment";
+import { ThumbsUpButton } from "./ThumbsUpButton";
+import { Link } from "react-router";
+import { useState } from "react";
 
 export const CardPost = ({ post }) => {
-    return (
-        <article className={styles.card}>
-            <header className={styles.header}>
-                <figure className={styles.figure}>
-                    <img
-                        src={post.cover}
-                        alt={`Capa do post de titulo: ${post.title}`}
-                    />
-                </figure>
-            </header>
-            <section className={styles.body}>
-                <h2>{post.title}</h2>
-                <p>{post.body}</p>
-                <Link to={`/blog-post/${post.slug}`}>Ver detalhes</Link>
-            </section>
-            <footer className={styles.footer}>
-                <div className={styles.actions}>
-                    <div className={styles.action}>
-                        <ThumbsUpButton loading={false} />
-                        <p>
-                            {post.likes}
-                        </p>
-                    </div>
-                    <div className={styles.action}>
-                        <ModalComment />
-                        <p>
-                            {post.comments.length}
-                        </p>
-                    </div>
-                </div>
-                <Author author={post.author} />
-            </footer>
-        </article>
-    )
-}
+  const [likes, setLikes] = useState(post.likes);
+
+  const handleLikeButton = () => {
+    fetch(`http://localhost:3000/blog-posts/${post.id}/like`, {
+      method: "POST",
+    }).then((response) => {
+      if (response.ok) {
+        setLikes((oldState) => oldState + 1);
+      }
+    });
+  };
+  return (
+    <article className={styles.card}>
+      <header className={styles.header}>
+        <figure className={styles.figure}>
+          <img src={post.cover} alt={`Capa do post de titulo: ${post.title}`} />
+        </figure>
+      </header>
+      <section className={styles.body}>
+        <h2>{post.title}</h2>
+        <p>{post.body}</p>
+        <Link to={`/blog-post/${post.slug}`}>Ver detalhes</Link>
+      </section>
+      <footer className={styles.footer}>
+        <div className={styles.actions}>
+          <div className={styles.action}>
+            <ThumbsUpButton loading={false} onClick={handleLikeButton} />
+            <p>{likes}</p>
+          </div>
+          <div className={styles.action}>
+            <ModalComment />
+            <p>{post.comments.length}</p>
+          </div>
+        </div>
+        <Author author={post.author} />
+      </footer>
+    </article>
+  );
+};
